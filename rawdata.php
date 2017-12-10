a0e8687c5f27a0d8d108283fb2f17132ba13e0dc
FloatingFox <33836286+FloatingFox@users.noreply.github.com>
2017-12-10 18:19:10 +0100

    Add files via upload

--- rawdata.php
+++ rawdata.php
@@ -1,57 +1,90 @@
-<html lang="en">
+
+<html>
   <head>
     <meta charset="utf-8">
     <title>CPi | RAW Data</title>
   </head>
- </html>
-
-<?php require "login/loginheader.php"; ?>
-<?php include 'navbar.php'; ?>
-<?php $config = include 'conf.php'; ?>
-
-<html>
   <link rel='shortcut icon' type='image/x-icon' href='icon/favicon.ico' />
   <link rel="stylesheet" href="css/cannapi.css">
 <body>
 
-<?php
+<script type="text/javascript">
+     
+        function link(target) {
+          if(confirm('Do you realy want to delete this record?'))
+            top.location.href = target;
+        }
+    
+    </script>
+	
+<?php 
+
+require "login/loginheader.php";
+include 'navbar.php';
+$config = include 'conf.php';
 
 $dsn = $config['host'].':'.$config['port'];
-//GET CURRENT DATE AND ADD TIME 00:00:00
-
-
+ // CHECK IF LIGHTSCHEDULE IS SET
+if( isset($config['lightOn']) && isset($config['lightOff']) ){
+	
+}else{
+	$config['lightOn'] = "00:00:00";
+	$config['lightOff'] = "00:00:00";
+}
+//GET CURRENT DATE
+//CREATE FUNCTION TO COUNT IN Y-M-D
+//SAVE CURRENT DATE
 $currentDate =  date('Y-m-d');
-$Date = $currentDate.' 00:00:00';
-
-//SUBSTRACT 2 DAYS to CURRENT DATE
 $start_date = $currentDate;
 $date = DateTime::createFromFormat('Y-m-d',$start_date);
 
-$date->modify('-1 day');
+// SUBSTRACT AMOUNT OF DAYS FROM CONFIG
 
-//SAVE CURRENT DATE -2 DAYS IN NEW VARIABLE
-$dateMin3 = $date->format('Y-m-d').' 00:00:00';
+$date->modify('-'.$config['shownDays'].' day');
+$date->modify('+1 day');
+$dateMin = $date->format('Y-m-d').' '.$config['lightOn'];
+
+
+//GET CURRENT DATE
+//CREATE FUNCTION TO COUNT IN Y-M-D
+//SAVE CURRENT DATE
+$currentDate =  date('Y-m-d');
+$start_date = $currentDate;
+$date = DateTime::createFromFormat('Y-m-d',$start_date);
+
+
+//SAVE CURRENT DATE +1 IN NEW VARIABLE
+$date->modify('+1 day');
+$datePlus = $date->format('Y-m-d').' '.$config['lightOff'];
+
 
 $connection = mysql_connect($dsn, $config['username'], $config['password']); 
 mysql_select_db($config['db_name']);
-
-$query = 'SELECT * FROM Klima WHERE datetime BETWEEN "'.$dateMin3.'" and "2018-12-31 00:00:00" ORDER BY datetime DESC;';
+$query = 'SELECT * FROM '.$config['table'].' WHERE datetime BETWEEN "'.$dateMin.'" and "'.$datePlus.'"ORDER BY datetime DESC;';
 $result = mysql_query($query);
 
+
+if(isset($_GET['del']) and !empty($_GET['del'])){
+	
+	mysql_query('DELETE FROM `Klima` WHERE record_id = '.$_GET['del']);	
+	// echo "<br />";
+	// echo "Record deleted";
+	// echo "<br />";
+	header("Refresh:0; url=rawdata.php");
+	}
+    
 ?>
 
-
 	<br /><div class="raw-data">
-	<table class="raw-data"><tr><td class="output_table_collum_names">Date</td><td class="output_table_collum_names">Temperature</td><td class="output_table_collum_names">Humidity</td></tr>
+	<table class="raw-data"><tr><td class="output_table_collum_names">Date</td><td class="output_table_collum_names">Temperature</td><td class="output_table_collum_names">Humidity</td><td class="output_table_collum_names">Delete</td><td class="output_table_collum_names">Delete</td></tr>
 	<?php 
 		while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-		echo "<tr class='output_table_rows'><td>" . $row['datetime'] . "</td><td>" . $row['temperature'] . "</td><td>" . $row['humidity'] . "</td></tr>";  //$row['index'] the index here is a field name
+		echo "<tr class='output_table_rows'><td class='output_table_rows'>" . $row['datetime'] . "</td><td class='output_table_rows'>" . $row['temperature'] . "</td><td class='output_table_rows'>" . $row['humidity'] . "</td><td class='output_table_rows'> <a href='#' onclick='link(\"rawdata.php?del=". $row['record_id'] ."\");'>" . $row['record_id'] ."</a></td></td><td class='output_table_rows'><input type='checkbox' name='record_id' value='" . $row['record_id'] ."'></td></tr>";  //$row['index'] the index here is a field name
 		}
 		mysql_close(); //Make sure to close out the database connection humidity
 	?>
 	</table>
-	</div> 
-
-
+	</div>
 </body>
 </html>
+
